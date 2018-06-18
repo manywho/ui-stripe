5391906a37735565666b810ac671b816327df0b0
Jose Collazzi <josecollazzi@gmail.com>
2018-06-18 15:09:36 +0100

    first commit

---
+++ src/pay-with-card-stripe.js
@@ -0,0 +1,134 @@
+import * as React from 'react';
+import './pay-with-card-stripe.css';
+import update from 'immutability-helper';
+
+import {injectStripe, CardElement, StripeProvider, Elements} from 'react-stripe-elements';
+
+const handleBlur = () => {
+    console.log('[blur]');
+};
+const handleChange = (change) => {
+    console.log('[change]', change);
+};
+const handleClick = () => {
+    console.log('[click]');
+};
+const handleFocus = () => {
+    console.log('[focus]');
+};
+const handleReady = () => {
+    console.log('[ready]');
+};
+
+const createOptions = (fontSize) => {
+    return {
+        style: {
+            base: {
+                fontSize: "16px",
+                color: '#32325d',
+                fontFamily: '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"',
+                '::placeholder': {
+                    color: "#868e96",
+                },
+            },
+            invalid: {
+                color: '#9e2146',
+            },
+        },
+    };
+};
+
+class _CardForm extends React.Component {
+    constructor() {
+        super();
+    }
+
+    handleSubmit = (ev) => {
+        ev.preventDefault();
+        const componentId = this.props.componentId;
+        const flowKey = this.props.flowKey;
+        if (this.props.stripe) {
+            this.props.stripe
+                .createToken()
+                .then((payload) => {
+                    console.log('[token]', payload);
+                    const model = manywho.model.getComponent(componentId, flowKey);
+                    const data = model.objectData;
+                    const newDataCopy = update(data, {0: {"properties": {1: {"contentValue": {$set: payload.token.id}}}}});
+                    manywho.state.setComponent(componentId, { objectData: newDataCopy }, flowKey, true,);
+
+                });
+        } else {
+            console.log("Stripe.js hasn't loaded yet.");
+        }
+    };
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Card details
+                    <CardElement
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        onFocus={handleFocus}
+                        onReady={handleReady}
+                        {...createOptions(this.props.fontSize)}
+                    />
+                </label>
+                <button>Pay</button>
+            </form>
+        );
+    }
+}
+
+const CardForm = injectStripe(_CardForm);
+
+class Checkout extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            elementFontSize: window.innerWidth < 450 ? '14px' : '18px',
+        };
+        window.addEventListener('resize', () => {
+            if (window.innerWidth < 450 && this.state.elementFontSize !== '14px') {
+                this.setState({elementFontSize: '14px'});
+            } else if (
+                window.innerWidth >= 450 &&
+                this.state.elementFontSize !== '18px'
+            ) {
+                this.setState({elementFontSize: '18px'});
+            }
+        });
+    }
+
+    render() {
+        const {elementFontSize} = this.state;
+        return (
+            <div className="Checkout">
+                <Elements>
+                    <CardForm fontSize={elementFontSize} componentId={this.props.componentId} flowKey={this.props.flowKey} />
+                </Elements>
+            </div>
+        );
+    }
+}
+
+class PayWithCardStripe extends React.Component {
+    render() {
+        const model = manywho.model.getComponent(this.props.id, this.props.flowKey);
+        const public_token= model.objectData[0].properties.filter(p => p.developerName === "Public Token")[0].contentValue;
+
+
+        return (
+            <div className="stripe-component">
+                <StripeProvider apiKey={public_token} >
+                    <Checkout componentId={this.props.id} flowKey={this.props.flowKey} />
+                </StripeProvider>
+            </div>
+        );
+    }
+}
+
+manywho.component.register('pay-with-card-stripe', PayWithCardStripe);
+
+export default PayWithCardStripe;
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const WriteFilePlugin = require('write-file-webpack-plugin');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: 'custom-components.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader',
+                ],
+            },
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+            { 
+                test:/\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                }) 
+            }
+        ]
+    },
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    },
+    plugins: [
+        new WriteFilePlugin(),
+        new ExtractTextPlugin("custom-components.css"),
+    ]
+};
